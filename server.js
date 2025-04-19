91007df8d3ac2bdffc023c948943250865a9e722
MatheusALehamnn <alexanderlehmannmd@icloud.com>
2025-04-19 18:49:15 -0300

    Corrige duplicação da variável PORT

--- server.js
+++ server.js
@@ -53,8 +53,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
-});
